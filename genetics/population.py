ef3878e9a6691d20ff807285fe3997a776204854
Ismael Taboada Rodero <ismaeljose.taboada@alumnos.uva.es>
2015-12-15 10:44:41 +0100

    Create log system.
    Keep elitism, crossing and mutation information of each generation.
    Log file `ga_n_queens.log`.

--- genetics/population.py
+++ genetics/population.py
@@ -14,8 +14,16 @@ Authors:
 
 from random import randint, random
 from genome import Genome
+import logging
+import os
 
 DEBUG = False
+LOG_FILE = 'ga_n_queens.log'
+
+if os._exists(LOG_FILE):
+    os.system('rm '+LOG_FILE)
+
+logging.basicConfig(filename=LOG_FILE, filemode='w', level=logging.DEBUG)
 
 
 class Population:
@@ -32,7 +40,7 @@ class Population:
         p_m(float): probability of mutation
     """
 
-    def __init__(self, N_chessboard, n_population, elitism=False, elite_len=5, p_c=0.75, p_m=0.02):
+    def __init__(self, N_chessboard, n_population, elitism=False, elite_len=6, p_c=0.7, p_m=0.05):
         """Initialize class instance.
 
         Arguments:
@@ -60,7 +68,13 @@ class Population:
         Returns:
             list: set of random generated fenotypes
         """
-        return [Genome([randint(1, N_chessboard) for _ in range(N_chessboard)]) for _ in range(n_population)]
+        population = []
+        while len(population) < n_population:
+            rand_chromosome = [randint(1, N_chessboard) for _ in range(N_chessboard)]
+            while rand_chromosome in population:
+                rand_chromosome = [randint(1, N_chessboard) for _ in range(N_chessboard)]
+            population.append(Genome(rand_chromosome))
+        return population
 
     def evolve(self, max_generation, finish_condition=lambda x: False, DEBUG=False):
         """Evolution process of population.
@@ -71,14 +85,14 @@ class Population:
             max_generation(int): maximum number of generations
             finish_condition(function): finish con
         """
-        if DEBUG:
-            print """
-========================
-Initial generation:
-            """
-            for index, fenotype in enumerate(self.generation):
-                print "-------------\nFenotype #{}:\n{}\nFitness:{}\n".format(index+1, fenotype,
-                                                                                fenotype.calculateFitness())
+#         if DEBUG:
+#             print """
+# ========================
+# Initial generation:
+#             """
+#             for index, fenotype in enumerate(self.generation):
+#                 print "-------------\nFenotype #{}:\n{}\nFitness:{}\n".format(index + 1, fenotype,
+#                                                                               fenotype.calculateFitness())
 
         for n_generation in range(max_generation):  # Iteration for each generation
             next_generation = []
@@ -86,7 +100,11 @@ Initial generation:
 
             if self.elitism:  # Resolve elitism
                 self.generation.sort(key=lambda gen: gen.calculateFitness())  # sorted by fitness
-                next_generation.extend([self.generation.pop(0) for _ in self.elite_len])
+                next_generation.extend([self.generation.pop(0) for _ in range(self.elite_len)])
+                if DEBUG:
+                    logging.debug("Elitism: Elite: [%s]",
+                                  ','.join(["[%s](%d)" % (','.join(map(str, elem.chromosome)), elem.calculateFitness())
+                                            for elem in next_generation]))
 
             while len(next_generation) < self.fenotypes:  # Until next generation if fill
 
@@ -94,22 +112,44 @@ Initial generation:
 
                 if random() < self.p_c:  # If crossing
                     child_a, child_b = parent_a.crossOver(parent_b)  # Generate offspring
+                    if DEBUG:
+                        logging.debug("Crossing: Parents: [%s](%d), [%s](%d)",
+                                      ','.join(map(str, parent_a.chromosome)), parent_a.calculateFitness(),
+                                      ','.join(map(str, parent_b.chromosome)), parent_b.calculateFitness())
+                        logging.debug("Crossing: Offspring: [%s](%d), [%s](%d)",
+                                      ','.join(map(str, child_a.chromosome)), child_a.calculateFitness(),
+                                      ','.join(map(str, child_b.chromosome)), child_b.calculateFitness())
                     sum_cross += 1
                     # Mutate offspring
-                    if random() < self.p_m: child_a.mutate(); sum_mut+=1
-                    if random() < self.p_m: child_b.mutate(); sum_mut+=1
+                    if random() < self.p_m:
+                        child_a.mutate()
+                        sum_mut += 1
+                        if DEBUG: logging.debug(
+                                "Mutation: Child_a: [%s](%d)", ','.join(map(str, child_a.chromosome)),
+                                child_a.calculateFitness())
+                    if random() < self.p_m:
+                        child_b.mutate()
+                        sum_mut += 1
+                        if DEBUG:
+                            logging.debug(
+                                    "Mutation: Child_a: [%s](%d)", ','.join(map(str, child_b.chromosome)),
+                                    child_b.calculateFitness())
                     next_generation.extend([child_a, child_b])  # Introduce into next generation
 
                 else:  # If not crossing, parents copy to next generation
+                    if DEBUG:
+                        logging.debug("Copied: Parents: [%s](%d), [%s](%d)",
+                                      ','.join(map(str, parent_a.chromosome)), parent_a.calculateFitness(),
+                                      ','.join(map(str, parent_b.chromosome)), parent_b.calculateFitness())
                     next_generation.extend([parent_a, parent_b])
 
             self.generation = []  # Next_generation as actual population
             self.generation.extend(next_generation)
 
-            fitnesses = map(lambda x: x.calculateFitness(),self.generation)
+            fitness = map(lambda x: x.calculateFitness(), self.generation)
 
-            min_fitness = min(fitnesses)  # Calculate minimun fitness value
-            avg_fitness = float(sum(fitnesses))/float(len(fitnesses))
+            min_fitness = min(fitness)  # Calculate minimun fitness value
+            avg_fitness = float(sum(fitness)) / float(len(fitness))
 
             if min_fitness == 0:  # Evaluate optimal solution at any fenotypes
                 break
@@ -117,10 +157,14 @@ Initial generation:
             if finish_condition(self.generation):  # Evaluate finish statement
                 break
             if DEBUG:
-                print "Generación#{} min_fitness:{} avg_fitness:{}\n\tcruces:{} mutaciones:{}".format(n_generation, min_fitness, avg_fitness, sum_cross, sum_mut)
+                logging.info("Generación#%d min_fitness:%d avg_fitness:%d\n\tcruces:%d mutaciones:%d", n_generation,
+                             min_fitness,
+                             avg_fitness,
+                             sum_cross,
+                             sum_mut)
                 # print """
-# ========================
-# Generation #{}:
+                # ========================
+                # Generation #{}:
                 # """.format(n_generation)
                 # for index, fenotype in enumerate(self.generation):
                 #    print "-------------\nFenotype #{}:\n{}\nFitness:{}\n".format(index, fenotype,
@@ -144,7 +188,7 @@ Initial generation:
             fenotypes.append(randint(0, len(self.generation) - 1))  # get random index to fenotype
         genome1 = self.generation.pop(min(fenotypes))  # extract the fenotype with best fitness
 
-        #genome1 = self.generation.pop(sorted(fenotypes,key=lambda x: x.calculateFitness()))  # extract the fenotype with best fitness
+        # genome1 = self.generation.pop(sorted(fenotypes,key=lambda x: x.calculateFitness()))  # extract the fenotype with best fitness
 
         # roulette selection
         totalFitness = 0.0
